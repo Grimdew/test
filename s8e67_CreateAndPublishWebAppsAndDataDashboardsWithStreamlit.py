94bcf74eb3d32971b46f6ba0b5bed2d50f2a9a68
Grimdew <124798704+Grimdew@users.noreply.github.com>
2023-02-08 08:24:57 -0600

    Add files via upload

---
+++ s8e67_CreateAndPublishWebAppsAndDataDashboardsWithStreamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Our First Streamlit App")
+st.subheader("Introducing Streamlit in automate Everythin with Python")
+st.write("""This is our fisrt web app.
+Enjoy it!
+""")
+
+#To run this program run this in the terminal: 
+# streamlit run Automate_Everything_with_PY\s8_ModernPythonTools\s8e67_CreateAndPublishWebAppsAndDataDashboardsWithStreamlit.py
