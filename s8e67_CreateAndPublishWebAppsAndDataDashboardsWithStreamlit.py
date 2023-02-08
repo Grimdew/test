3c483dfc421039ae4204a08d7e8786a339c9e84b
Grimdew <124798704+Grimdew@users.noreply.github.com>
2023-02-08 08:44:59 -0600

    Add table

--- s8e67_CreateAndPublishWebAppsAndDataDashboardsWithStreamlit.py
+++ s8e67_CreateAndPublishWebAppsAndDataDashboardsWithStreamlit.py
@@ -1,10 +1,21 @@
 import streamlit as st
+import pandas
+
+
+data = {
+    'Series_1': [1, 3, 4, 5, 7],
+    'Series_2': [10, 30, 40, 100, 250]
+}
+
+df = pandas.DataFrame(data)
 
 st.title("Our First Streamlit App")
 st.subheader("Introducing Streamlit in automate Everythin with Python")
 st.write("""This is our fisrt web app.
 Enjoy it!
 """)
+
+st.write(df)
 
 #To run this program run this in the terminal: 
 # streamlit run Automate_Everything_with_PY\s8_ModernPythonTools\s8e67_CreateAndPublishWebAppsAndDataDashboardsWithStreamlit.py
